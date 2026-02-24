b9348a94be4b94a5b7a67712f3d85716042d5434
rlarson20 <richard.rj.larson@gmail.com>
2026-02-24 12:58:09 -0500

    chore: add `nom` dependency

--- crates/elbrus-core/src/mana.rs
+++ crates/elbrus-core/src/mana.rs
@@ -4,6 +4,9 @@ use smallvec::SmallVec;
 use std::sync::Arc;
 
 /// A single symbol: {W}, {2/U}, {X}, {T}, {C}, {CHAOS}, etc.
+/// TODO: not all symbols are mana symbols
+/// put into a separate enum
+/// and just combine into Symbol enum
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
 #[non_exhaustive]
 pub enum ManaSymbol {
@@ -13,8 +16,8 @@ pub enum ManaSymbol {
     Colorless, // C
     Snow,      // S
     Hybrid(Color, Color),
-    MonoHybrid(Color), // {2/W}
-    Phyrexian(Color),  // {W/P}
+    TwoBrid(Color),   // {2/W}
+    Phyrexian(Color), // {W/P}
     HybridPhyrexian(Color, Color),
     Tap, // {T} (for reminder text)
     Unknown(Arc<str>),
